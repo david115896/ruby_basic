6b396d38622ed74d60f3b22cdbd157b36b333a5b
david115896 <dtaccori@gmail.com>
2019-10-03 16:16:34 +0200

    add exercise 3 with graphic

---
+++ lib/03_stairway_graphique.rb
@@ -0,0 +1,55 @@
+$num=0
+$pos=0
+$tours=0
+$stat=0
+$table_stat=[]
+def random
+	#nex=gets.chomp	
+	$num=rand(1..6)
+	puts"Vous avez lance le de, et obtenu #{$num}"
+end
+
+def position(num)
+$tours=$tours+1
+if num==5 || num==6
+	$pos=$pos+1
+	puts "Votre nouvelle position est #{$pos}"
+elsif num==1 && $pos!=0
+	$pos=$pos-1 
+        puts "Votre nouvelle position est #{$pos}"
+else	
+	puts"Dommage vous n'avez pas avance, vous reste a la position #{$pos}"
+end
+
+if 	$pos==10 
+	puts "Bravo, vous etes arrive en haut de l'escalier"
+end
+end
+def half_pyramid
+n=10
+
+1.upto(n).each{|x|puts (if x==10-$pos then "&"+"#"*x else"#"*x end  ).rjust(n+1)}
+
+end
+
+def statistic
+	sum=0
+	$table_stat.each{|x| sum=sum+x}
+	return  "La moyenne de 100 partie est #{sum/100} tours pour arriver en haut de l'escalier"
+		
+end
+def perform
+	100.times do 	
+		while $pos!=10
+		random
+		position($num)
+		half_pyramid
+		end
+	$table_stat << $tours
+	$tours=0
+	$pos=0
+	end
+	puts statistic
+	#puts $table_stat.size
+end
+perform
